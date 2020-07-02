b6d9ab7a7ff87a90081318ca3ac0200e2e5ad2be
JoshuaKeegan3 <37237625+JoshuaKeegan3@users.noreply.github.com>
2020-07-02 23:44:24 +1200

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,11 @@
+# Find the 1,000,000th lexicographic permutation of the integer set 0-9
+n = 0
+i = 0
+numbers = set([i for i in range(10)])
+numbers2 = set([i for i in range(1,10)])
+while n< 1000000:
+   if all((str(num) in str(i) for num in numbers)) or all((str(num2) in str(i) and len(str(i)) < len(numbers) for num2 in numbers2)):
+      n+=1
+   i +=1
+   
+print(n)
