5657a78dbf6a91921c39607a313b6f1bdf40988d
JoshuaKeegan3 <37237625+JoshuaKeegan3@users.noreply.github.com>
2020-07-03 17:28:51 +1200

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,12 +1,9 @@
 '''
 #The sum of the squares of the first ten natural numbers is,
-
 #12 + 22 + ... + 102 = 385
 #The square of the sum of the first ten natural numbers is,
-
 #(1 + 2 + ... + 10)2 = 552 = 3025
 #Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 '''
 total_difference = 0
@@ -16,5 +13,5 @@ for i in range(1,101):
     total1 += i ** 2
     total2 += i
 total2 **= 2
-total_difference = total1-total2
-print(total_difference ** 2)
+total_difference = total2-total1
+print(total_difference)
